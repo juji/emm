b3776d7c6c1a7a453df809c131f719d5f4967e12
juji <jujiyangasli@gmail.com>
2025-04-18 04:00:39 +0700

    asdf

--- src/types/archy.d.ts
+++
@@ -1,8 +0,0 @@
-declare module 'archy' {
-  interface ArchyNode {
-    label: string;
-    nodes?: Array<ArchyNode | string>;
-  }
-
-  export default function archy(data: ArchyNode, prefix?: string, opts?: any): string;
-}
